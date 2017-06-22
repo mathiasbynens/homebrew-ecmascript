bb870f7f1348bbdc967a5cf7ec73c82a622d0f52
Mathias Bynens <mathias@qiwi.be>
2017-06-22 10:09:03 +0200

    Test Travis CI failure

--- Formula/chakra.rb
+++ Formula/chakra.rb
@@ -2,7 +2,7 @@ class Chakra < Formula
   desc "The core part of the Chakra JavaScript engine that powers Microsoft Edge"
   homepage "https://github.com/Microsoft/ChakraCore"
   url "https://aka.ms/chakracore/cc_osx_x64_1_5_1"
-  sha256 "8e85cfc21a693ae1b02373228a3a921485aa6567faa97dcdb904e030632b908b"
+  sha256 "1e85cfc21a693ae1b02373228a3a921485aa6567faa97dcdb904e030632b908b"
 
   def install
     lib.install "lib/libChakraCore.dylib"
